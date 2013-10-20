5c6eec8d89be802e561e3e1ee6f98b7e746b7b18
Adam Compton <comptona@gmail.com>
2013-10-19 22:18:24 -0700

    Improved metric labeling
    Now the plugin will read `device/tempX_label` files (if they exist)
    in order to get more descriptive metric names. Also, the initial
    code was pretty gross and brittle, this should be better.

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ A python gmond module that reads /sys/class/hwmon.
 ![Demo image](https://raw.github.com/handyman5/gmond-hwmon-module/screenshots/hwmon.png)
 
 It should automatically figure out what sensors exist and give the metrics reasonable names.
+
+**NEW**: This module will now read the `device/tempX_label` files (if they exist) to get more descriptive metric names.
--- hwmon.py
+++ hwmon.py
@@ -2,13 +2,12 @@
 
 root = '/sys/class/hwmon'
 descriptors = []
+mapping = {}
 
 import os, glob, re
 
 def temp_finder(name):
-    params = re.search (r'.*_dev(\d+)_temp(\d+)_input', name).groups()
-    path = os.path.join(root, 'hwmon%d/device/temp%d_input' % (int(params[0]), int(params[1])))
-    val = open(path).read().strip()
+    val = open(mapping[name]).read().strip()
     return int(val) / 1000.0
 
 def metric_init(params):
@@ -18,14 +17,23 @@ def metric_init(params):
 
     for s in sensors:
         temps = glob.glob(os.path.join(s, 'device/temp*_input'))
-        probes = zip([s.replace('input', 'label') for x in temps], temps)
+        # dict values are default labels if no label files exist
+        probes = dict(zip(temps, [os.path.basename(x) for x in temps]))
 
-        for i, l in probes:
-            l = os.path.basename(l)
+        for i in probes.keys():
+            try:
+                fname = i.replace('input', 'label')
+                fhandle = open(fname, 'r')
+                probes[i] = fhandle.read().strip().replace(' ', '_').lower()
+                fhandle.close()
+            except (IOError, OSError):
+                pass
+
+        for i, l in probes.iteritems():
             num = re.search('\d+', i)
             device = i[num.start():num.end()]
-            label = l.replace(' ', '_').lower()
-            item = {'name': 'hwmon_dev%s_%s' % (device, label),
+            name = 'hwmon_dev%s_%s' % (device, l)
+            item = {'name': name,
                     'call_back': temp_finder,
                     'time_max': 90,
                     'value_type': 'float',
@@ -35,6 +43,7 @@ def metric_init(params):
                     'description': 'Temperature for hwmon probe %s' % l,
                     'groups': 'hwmon'}
             descriptors.append(item)
+            mapping[name] = i
 
     return descriptors
 
@@ -46,4 +55,4 @@ if __name__ == '__main__':
     metric_init(None)
     for d in descriptors:
         v = d['call_back'](d['name'])
-        print 'value for %s: %s' % (d['name'], str(v))
+        print 'value for %s: %s' % (d['name'],  str(v))
